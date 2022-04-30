350df67ddbbf7b56d31fd2eadec507731474c394
Suyeon-B <tndus4243@naver.com>
2022-04-30 21:56:39 +0900

    pointer 연습 & rbtree init

--- HelloWorld/HelloWorld.c
+++ HelloWorld/HelloWorld.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void){
-    printf("Hello World");
+int main(void)
+{
+    int a = 1;
+    int *ptr = &a;
+    printf("%d\n", a);
+    // printf("%p\n", &a);
+    printf("%d\n", *ptr); // 1
+
+    // printf("%p\n", &ptr);
+    printf("%p\n", &a);
     return 0;
 }
